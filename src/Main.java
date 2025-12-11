0d8fd691fb46bb12a62619a376cb4f91e6397f57
JayedFaruk <jayedf@nycstudents.net>
2025-12-11 15:25:01 -0500

    part 1

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,25 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        read();
+        System.out.println(read());
     }
 
     public static int read() throws FileNotFoundException {
         File f = new File("src/Puzzle.txt");
         Scanner s = new Scanner(f);
         int num = 50;
-        int t = 0;
         int count = 0;
+        String str = "";
         String lr = "";
         while (s.hasNext()) {
-            lr = s.next();
-            t = s.nextInt();
-            if (lr == "R") num += t;
-            else num -= t;
+            str = s.next();
+            lr = str.substring(0, 1);
+            int i = Integer.parseInt(str.substring(1));
+            if (i>100) i = i % 100;
+            if (lr.equals("R")) num += i;
+            else num -= i;
+            if (num >= 100) num %= 100;
+            if (num < 0) num += 100;
             if (num == 0) count++;
         }
         return count;
